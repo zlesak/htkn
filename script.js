c76280f43957f257722cd020045a8882f1946bab
Jan Zlesák <zlesak@spstrutnov.cz>
2022-10-13 11:35:03 +0200

    2
    2

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let skoly = "https://services6.arcgis.com/ogJAiK65nXL1mXAW/arcgis/rest/services/Seznam_škol_a_školských_zařízení/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json";
 let studentiVS = "https://services6.arcgis.com/ogJAiK65nXL1mXAW/arcgis/rest/services/Počty_studentů_krajských_vyšších_odborných_škol/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json";
 let studentiSS = "https://services6.arcgis.com/ogJAiK65nXL1mXAW/arcgis/rest/services/Počty_žáků_v_oborech_středních_škol_zřizovaných_krajem/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json";
-
+let druhy = [];
 let skolyData;
 
 async function getSkoly(){
@@ -17,6 +17,8 @@ async function getSkoly(){
         let td1 =  document.createElement("td");td1.innerHTML = att.nazev;
         let td2 = document.createElement("td"); td2.innerHTML = att.ico;
         let td3 = document.createElement("td"); td3.innerHTML = att.zarizeni_druh;
+        if(!druhy.includes(td3.innerHTML))
+            druhy.push(td3.innerHTML)
         let td4 = document.createElement("td"); td4.innerHTML = att.nazev_okresu;
         let td5 = document.createElement("td"); td5.innerHTML = att.nazev_obce;
         let td6 = document.createElement("td"); td6.innerHTML = att.nazev_ulice;
@@ -36,26 +38,9 @@ async function getSkoly(){
         tr.appendChild(td9);
         tr.appendChild(td10);
         tr.appendChild(td11);
-
-
         document.getElementById("main").appendChild(tr);
-    // tr.appendChild("td").innerHTML = skola.attributes.nazev;
-        /*
-        <tr v-for="dat in data" :key="dat.attributes.OBJECTID">
-            <td>dat.attributes.nazev</td>
-            <td>dat.attributes.ico</td>
-            <td>dat.attributes.zarizeni_druh</td>
-            <td>dat.attributes.nazev_okresu</td>
-            <td>dat.attributes.nazev_obce</td>
-            <td>dat.attributes.nazev_ulice</td>
-            <td>č.p. dat.attributes.cislo_domovni</td>
-            <td>dat.attributes.psc</td>
-            <td>dat.attributes.www</td>
-            <td>dat.attributes.x</td>
-            <td>dat.attributes.y</td>
-        </tr>
-        */
     });
+    console.log(druhy);
 }
 
 
